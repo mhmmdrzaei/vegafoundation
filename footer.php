5845c99eb786a7d0c77378e5f52d82000e35d74d
Mohammad <mhmmdrzaei@gmail.com>
2023-02-17 15:50:34 -0500

    header and footer cleanup

--- footer.php
+++ footer.php
@@ -1,18 +1,32 @@
 <footer>
-  <div class="container">
-    <p>&copy; Project Name <?php echo date('Y'); ?></p>
-  </div>
+    <section class="logo">
+
+    </section>
+    <section class="vegaInfo">
+
+    </section>
+    <section class="contact">
+        <p>&copy; <?php bloginfo( 'name' ); ?> <?php echo date('Y'); ?></p>
+    </section>
 </footer>
 
 <script>
 // scripts.js, plugins.js and jquery are enqueued in functions.php
 /* Google Analytics! */
- var _gaq=[["_setAccount","UA-XXXXX-X"],["_trackPageview"]]; // Change UA-XXXXX-X to be your site's ID
- (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
- g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
- s.parentNode.insertBefore(g,s)}(document,"script"));
+var _gaq = [
+    ["_setAccount", "UA-XXXXX-X"],
+    ["_trackPageview"]
+]; // Change UA-XXXXX-X to be your site's ID
+(function(d, t) {
+    var g = d.createElement(t),
+        s = d.getElementsByTagName(t)[0];
+    g.async = 1;
+    g.src = ("https:" == location.protocol ? "//ssl" : "//www") + ".google-analytics.com/ga.js";
+    s.parentNode.insertBefore(g, s)
+}(document, "script"));
 </script>
 
 <?php wp_footer(); ?>
 </body>
+
 </html>
